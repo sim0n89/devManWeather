50b71ccaa11e18ecfa822008b9446721911f592b
Виталий Ильин <sima26ru@gmail.com>
2023-06-23 19:02:56 +0300

    readme.md и рефактор

--- main.py
+++ main.py
@@ -3,44 +3,23 @@ import requests
 
 def get_weather(city, params=None):
     try:
-        response = requests.get("https://wttr.in/" + city, params=params)
+        response = requests.get(f"https://wttr.in/{city}", params=params)
         response.raise_for_status()
         print(response.text)
     except Exception as e:
-        print("Error: " + str(e))
+        print("Упс! Произошла ошибка!")
         return
 
 
-
 if __name__ == '__main__':
-    weatherUrls = [
-        {
-            "city": "san%20francisco",
-            "params": {'nTqu': "",
-                       "lang": "ru", }
-        },
-        {
-            "city": "svo",
-            "params": {'nTqu': "",
-                       "lang": "ru", }
-        },
-        {
-            "city": "london",
-            "params": {'nTqu': "",
-                       "lang": "ru", }
-        },
-        {
-            "city": "череповец",
-            "params": {"m": "",
-                       "lang": "ru",
-                       "M": "",
-                       "n": "",
-                       "q": "",
-                       "T": ""
-                       }
-        }
-    ]
-
-    for url in weatherUrls:
-        get_weather(url["city"], url["params"])
+    params = {"m": "",
+              "lang": "ru",
+              "M": "",
+              "n": "",
+              "q": "",
+              "T": ""
+              }
+    citys = ["san%20francisco", "svo", "london", "череповец", ]
+    for city in citys:
+        get_weather(city, params)
         pass
